0d428436656de7f6e56df33959d0688ed0f33214
Matheus Perrut <matheus.perrut2302@gmail.com>
2019-08-01 01:03:11 -0300

    Criação do scaffold de categorias

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_31_015327) do
+ActiveRecord::Schema.define(version: 2019_08_01_021319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "drums", force: :cascade do |t|
+  create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
