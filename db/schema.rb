5bb498ffe39bd318cb4282759812aab9b6febe60
Matheus Perrut <matheus.perrut2302@gmail.com>
2020-03-04 02:20:14 -0300

    - Voltando a salvar endereço de cobrança do usuário - Adicionando Bairro ao Usuário

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_02_070046) do
+ActiveRecord::Schema.define(version: 2020_03_04_015037) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -135,6 +135,7 @@ ActiveRecord::Schema.define(version: 2020_03_02_070046) do
     t.string "customer_wirecard_id"
     t.string "birthdate"
     t.string "own_id_wirecard"
+    t.string "bairro"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
