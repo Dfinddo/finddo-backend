dabd7acb40b35eebe61054cd07753cc20cb1c9e2
Matheus Perrut <matheus.perrut2302@gmail.com>
2020-05-13 23:17:35 -0300

    - Correção de url de dev para email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "https://www.finddo.com.br" }
+  config.action_mailer.default_url_options = { host: "http://localhost:4200" }
 
   # config.action_mailer.default_url_options = { host: 'mailcatcher' }
   # config.action_mailer.delivery_method = :smtp
