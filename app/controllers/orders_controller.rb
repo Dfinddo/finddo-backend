bd10e943fc8bff24ccff657e1b42360df613cd94
Matheus Perrut <matheus.perrut2302@gmail.com>
2020-06-18 19:20:20 -0300

    - Removendo statements de debug de notificações

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -129,15 +129,13 @@ class OrdersController < ApplicationController
       end
 
       if devices.length > 0
-        req = HTTParty.post("https://onesignal.com/api/v1/notifications", 
+        HTTParty.post("https://onesignal.com/api/v1/notifications", 
             body: { 
               app_id: ENV['ONE_SIGNAL_APP_ID'], 
               include_player_ids: devices,
               data: {pedido: 'novo'},
               contents: {en: "Novo pedido disponível para atendimento"} })
         
-        print req.code
-        print "==================================================================="
       end
 
       render json: @order, status: :created
