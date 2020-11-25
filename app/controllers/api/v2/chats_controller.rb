c2f1b37925125c84c1736d82cf9058d101901c41
Ariel <mileguir23@hotmail.com>
2020-11-25 11:36:47 -0300

    Chat otimizado, código melhorado e tempo de resposta muito mais rapido.

--- app/controllers/api/v2/chats_controller.rb
+++ app/controllers/api/v2/chats_controller.rb
@@ -1,12 +1,19 @@
 class Api::V2::ChatsController < Api::V2::ApiController
    before_action :require_login
    before_action :set_chat, only: [:show]
-   before_action :check_admin, only: [:index, :show]
    before_action :set_user_services, only: [:get_chat_list, :get_chat_with_admin_list, :for_admin_get_chat_list]
    
    #GET /chats?page
    def index
      page = params[:page].to_i
+     
+     chats = nil
+     total = nil
+     
+     if check_admin != 200
+      render json: {"error": "Error: Admin privileges required."}
+      return 400
+     end
 
      if page == 0
       page = 1
@@ -34,6 +41,16 @@ class Api::V2::ChatsController < Api::V2::ApiController
    def get_by_order
     page = params[:page].to_i
 
+    order = nil
+    chats = nil
+    total = nil
+    session_user_type = nil
+    session_user_id = nil
+    sender_id = nil
+    receiver_id = nil
+    order_status = nil
+    response = nil
+
     if page == 0
       page = 1
     elsif page < 0
@@ -46,26 +63,35 @@ class Api::V2::ChatsController < Api::V2::ApiController
     .where(for_admin: 0)
     .order(created_at: :desc).page(page)
     
-    total_pages = chats.total_pages
+    total = chats.total_pages
 
-    if total_pages == 0
+    if total == 0
       render json: {"error": "Error: required order doesn't have any chats associated with it."}
       return 400
-    elsif page > total_pages
+    elsif page > total
       render json: {"error": "Error: page is greater then total_pages."}
       return 400
     end
 
+    session_user_type = check_user_type
+    session_user_id = session_user.id
+
     chat_sample = chats[0]
-    if ((session_user.id != chat_sample.sender_id && session_user.id != chat_sample.receiver_id) && session_user.user_type != "admin")
+    
+    sender_id = chat_sample.sender_id
+    receiver_id = chat_sample.receiver_id
+
+    order_status = order.order_status
+
+    if ((session_user_id != sender_id && session_user_id != receiver_id) && session_user_type != 3)
       render json: {"error": "Current user doesn't have permission to acess this."}
       return 400
     end
     
-    if (session_user.user_type != "admin" && (order.order_status == "finalizado" || order.order_status == "cancelado" || order.order_status == "analise"))
+    if (session_user_type != 3 && (order_status == "finalizado" || order_status == "cancelado" || order_status == "analise"))
       response = {"chats": [], "current_page": 1, "total_pages": 1}
     else
-      response = { chats: chats.map { |chat| ChatSerializer.new(chat) }, current_page: chats.current_page, total_pages: chats.total_pages }
+      response = { chats: chats.map { |chat| ChatSerializer.new(chat) }, current_page: chats.current_page, total_pages: total }
     end
 
     render json: response
@@ -76,6 +102,16 @@ class Api::V2::ChatsController < Api::V2::ApiController
    def admin_chat_from_order
     page = params[:page].to_i
 
+    order_id = nil
+    order = nil
+    receiver = nil
+    session_user_type = nil
+    session_user_id = nil
+    receiver_id = nil
+    receiver_user_type = nil
+    total = nil
+    response = nil
+
     if page == 0
       page = 1
     elsif page < 0
@@ -85,6 +121,8 @@ class Api::V2::ChatsController < Api::V2::ApiController
     
     order_id = params[:order_id].to_i
     order = Order.find_by(id: order_id)
+    session_user_type = check_user_type
+    session_user_id = session_user.id
 
     if order == nil
       render json: {"error": "Error: Order does not exist."}
@@ -92,45 +130,51 @@ class Api::V2::ChatsController < Api::V2::ApiController
     end
 
     receiver = User.find_by(id: params[:receiver_id].to_i)
+    receiver_id = receiver.id
+    receiver_user_type = receiver.user_type
 
-    if session_user.user_type == "user" || session_user.user_type == "professional" 
+    if session_user_type == 1 || session_user_type == 2
       
-      if receiver.user_type != "admin"
+      if receiver_user_type != "admin"
         render json: {"error": "Error: Receiver is not an admin."}
-        return
+        return 400
       end
 
-      if session_user.user_type == "user"
-        chats = Chat.where("sender_id = ? or sender_id = ?", session_user.id, receiver.id)
+      if session_user_type == 1
+        chats = Chat.where("sender_id = ? or sender_id = ?", session_user_id, receiver_id)
         .where(order_id: order_id)
-        .where("receiver_id = ? or receiver_id = ?", session_user.id, receiver.id)
+        .where("receiver_id = ? or receiver_id = ?", session_user_id, receiver_id)
         .where(for_admin: 1)
         .order(created_at: :desc).page(page)
 
       elsif session_user.user_type == "professional"
-        chats = Chat.where("sender_id = ? or sender_id = ?", session_user.id, receiver.id)
+        chats = Chat.where("sender_id = ? or sender_id = ?", session_user_id, receiver_id)
         .where(order_id: order_id)
-        .where("receiver_id = ? or receiver_id = ?", session_user.id, receiver.id)
+        .where("receiver_id = ? or receiver_id = ?", session_user_id, receiver_id)
         .where(for_admin: 2)
         .order(created_at: :desc).page(page)
 
       end
 
-    elsif session_user.user_type == "admin"
-
-      if receiver.user_type == "user"
-        chats = Chat.where("sender_id = ? or sender_id = ?", session_user.id, receiver.id)
+    elsif session_user_type == 3
+      
+      if receiver_user_type == "user"
+        chats = Chat.where("sender_id = ? or sender_id = ?", session_user_id, receiver_id)
         .where(order_id: order_id)
-        .where("receiver_id = ? or receiver_id = ?", session_user.id, receiver.id)
+        .where("receiver_id = ? or receiver_id = ?", session_user_id, receiver_id)
         .where(for_admin: 1)
         .order(created_at: :desc).page(page)
       
-      elsif receiver.user_type == "professional"
-        chats = Chat.where("sender_id = ? or sender_id = ?", session_user.id, receiver.id)
+      elsif receiver_user_type == "professional"
+        chats = Chat.where("sender_id = ? or sender_id = ?", session_user_id, receiver_id)
         .where(order_id: order_id)
-        .where("receiver_id = ? or receiver_id = ?", session_user.id, receiver.id)
+        .where("receiver_id = ? or receiver_id = ?", session_user_id, receiver_id)
         .where(for_admin: 2)
         .order(created_at: :desc).page(page)
+
+      else
+        render json: {"debug": "Error: Messages between admins."}
+        return 400  
 
       end
 
@@ -154,6 +198,20 @@ class Api::V2::ChatsController < Api::V2::ApiController
 
    #GET /chats/list?page
    def get_chat_list
+     orders = nil
+     total = nil
+     order = nil
+     order_id = nil
+     last_chat = nil
+     receiver = nil
+     receiver_id = nil
+     receiver_profile_photo = nil
+     service_type = nil
+     title = nil
+     last_message = nil
+     
+     session_user_id = session_user.id
+     session_user_type = check_user_type
      page = params[:page].to_i
      list = []
 
@@ -164,7 +222,12 @@ class Api::V2::ChatsController < Api::V2::ApiController
        return 400
      end
 
-     orders = Order.where("user_id = ? OR professional = ?", session_user.id, session_user.id)
+     if session_user_type == 3
+      render json: {"debug": "Error: Admins should be using another function."}
+      return 400
+     end
+
+     orders = Order.where("user_id = ? OR professional = ?", session_user_id, session_user_id)
      .where.not(order_status: :finalizado)
      .where.not(order_status: :cancelado)
      .where.not(order_status: :analise)
@@ -191,9 +254,9 @@ class Api::V2::ChatsController < Api::V2::ApiController
        last_chat = Chat.where(order_id: order_id)
        .where(for_admin: 0).last
 
-       if session_user.user_type == "user"
+       if session_user_type == 1
         receiver_id = order.professional_order.id
-      elsif session_user.user_type == "professional"
+      elsif session_user_type == 2
         receiver_id = order.user.id
       else
         render json: {"error": "Error: This function is intended for users and professionals only."}
@@ -234,9 +297,25 @@ class Api::V2::ChatsController < Api::V2::ApiController
    end
 
    #GET /chats/admin/list?page&for_admin
-   def get_chat_with_admin_list
+   def get_chat_with_admin_list 
      page = params[:page].to_i
      list = []
+     session_user_type = check_user_type
+     session_user_id = session_user.id
+     
+     for_admin = nil
+     orders = nil
+     total = nil
+     order = nil
+     order_id = nil
+     last_chat = nil
+     sender = nil
+     receiver = nil
+     sender_user_type = nil
+     receiver_user_type = nil
+     receiver_profile_photo = nil
+     service_type = nil
+     title = nil
 
      if page == 0
       page = 1
@@ -245,10 +324,10 @@ class Api::V2::ChatsController < Api::V2::ApiController
       return 400
      end
      
-     if session_user.user_type == "user" || session_user.user_type == "professional"
+     if session_user_type == 1 || session_user_type == 2
 
        #Reescreve a váriavel for_admin recebida por parametro, caso o tipo de usuário seja user, ou professional.
-       if session_user.user_type == "user"
+       if session_user_type == 1
          for_admin = 1
        else
          for_admin = 2
@@ -256,7 +335,7 @@ class Api::V2::ChatsController < Api::V2::ApiController
 
        #Pega apenas os pedidos que já tiveram troca de mensagens entre usuário e admin
        orders = Order.joins("LEFT JOIN chats on (orders.id = order_id)")
-       .where("sender_id = ? OR receiver_id = ?",session_user.id, session_user.id)
+       .where("sender_id = ? OR receiver_id = ?",session_user_id, session_user_id)
        .where("chats.for_admin = ?", for_admin)
        .where.not(order_status: :analise)
        .distinct
@@ -362,10 +441,12 @@ class Api::V2::ChatsController < Api::V2::ApiController
 
          #Faz o título
          service_type = order.category.name + " - " +  name + " - "
+         receiver_user_type = receiver.user_type
+         sender_user_type = sender.user_type
 
-         if receiver.user_type  == "user" || sender.user_type == "user"
+         if receiver_user_type  == "user" || sender_user_type == "user"
            title = service_type + "Suporte (Usuário)"
-         elsif receiver.user_type == "professional" || sender.user_type == "professional"
+         elsif receiver_user_type == "professional" || sender_user_type == "professional"
            title = service_type + "Suporte (Profissional)"
          else
            render json: {"debug": "Error: This function logic is not prepared to deal with this type of user."}
@@ -404,6 +485,9 @@ class Api::V2::ChatsController < Api::V2::ApiController
     sender = session_user
 
     receiver = User.find_by(id: chat_params[:receiver_id])
+
+    order = nil
+    sender_id = nil
 
     if receiver == nil
       render json: {"error": "Error: Receiver could not be found."}
@@ -444,7 +528,11 @@ class Api::V2::ChatsController < Api::V2::ApiController
 
     sender = session_user
 
+    sender_user_type = check_user_type
+
     receiver = User.find_by(id: chat_params[:receiver_id])
+
+    receiver_user_type = receiver.user_type
 
     if receiver == nil
       render json: {"error": "Error: Receiver could not be found."}
@@ -453,7 +541,7 @@ class Api::V2::ChatsController < Api::V2::ApiController
   
     if (sender == receiver)
       render json: {"error": "Error: Sender can not be the receiver."}
-      return 401
+      return 400
     end
 
     #Pedido criado especialmente para se falar com o admin por fora de qualquer pedido
@@ -462,9 +550,9 @@ class Api::V2::ChatsController < Api::V2::ApiController
     sender_id = sender.id
     chat.sender_id = sender_id
 
-    if session_user.user_type == "user" || receiver.user_type == "user"
+    if sender_user_type == 1 || receiver_user_type == "user"
       chat.for_admin = 1
-    elsif session_user.user_type == "professional" || receiver.user_type == "professional"
+    elsif sender_user_type == 2 || receiver_user_type == "professional"
       chat.for_admin = 2
     else
       render json: {"debug": "Error: This function logic is not prepared to deal with this type of user."}
@@ -487,14 +575,24 @@ class Api::V2::ChatsController < Api::V2::ApiController
 
     #Essa função pode ser facilmente adaptada para mostrar a lista de conversas de um usuário com os administadores caso seja necessário.
     #Basta checar se o session_user é user, ou professional, e fazer um User.where(user_type: 0).order(created_at: :desc).page(page)
-    if session_user.user_type != "admin"
-      render json: {"error": "Error: admin privileges required."}
-      return
-    end
-  
+
+    #Definicao de variaveis
     page = params[:page].to_i
     list = []
+    users = nil
+    total = nil
+    user = nil
+    user_type = nil
+    chat = nil
+    title = nil
+    user_profile_photo = nil
+    last_message = nil
 
+    if check_admin != 200
+      render json: {"error": "Error: admin privileges required."}
+      return 400
+    end
+  
     if page == 0
       page = 1
     elsif page < 1
@@ -524,10 +622,15 @@ class Api::V2::ChatsController < Api::V2::ApiController
         next
       end
 
-      if user.user_type == "user"
+      user_type = user.user_type
+
+      if user_type == "user"
         title = "Suporte - " + user.name + " - (Usuário)"
-      else
+      elsif user_type == "professional"
         title = "Suporte - " + user.name + " - (Profissional)"
+      else
+        render json: {"debug": "Error: This function logic is not prepared to deal with this type of user."}
+        return 400
       end
 
       #Pega a foto do usuário
@@ -553,6 +656,7 @@ class Api::V2::ChatsController < Api::V2::ApiController
   def get_chat_with_admin
     page = params[:page].to_i
     receiver_id = params[:receiver].to_i
+    session_user_id = session_user.id
 
     if page == 0
       page = 1
@@ -562,7 +666,7 @@ class Api::V2::ChatsController < Api::V2::ApiController
     end
 
     if check_admin != 200
-      chats = Chat.where(order_id: 170).where("sender_id = ? OR receiver_id = ?",session_user.id, session_user.id)
+      chats = Chat.where(order_id: 170).where("sender_id = ? OR receiver_id = ?",session_user_id, session_user_id)
       .order(created_at: :desc).page(page)
     else
       chats = Chat.where(order_id: 170).where("sender_id = ? OR receiver_id = ?",receiver_id, receiver_id)
@@ -592,23 +696,19 @@ class Api::V2::ChatsController < Api::V2::ApiController
       params.require(:chat).permit(:message, :is_read, :sender_id, :receiver_id, :created_at, :updated_at, :order_id, :for_admin)
     end
 
-    def check_user_receiver
-      #Não está em uso, mas escrito caso necessário.
-      if (session_user != User.find(@chat.receiver_id) && session_user.user_type != "admin")
-        return 400
+    def check_user_type
+      type = session_user.user_type
+      if type == "user"
+        return 1
+      elsif type == "professional"
+        return 2
+      elsif type == "admin"
+        return 3
       end
 
-      return 200
+      return -1
     end
-
-    def check_user_sender
-      if (session_user != User.find(@chat.sender_id) && session_user.user_type != "admin")
-        return 400
-      end
-
-      return 200
-    end
-    
+        
     def check_admin
       if (session_user.user_type == "admin")
         return 200
