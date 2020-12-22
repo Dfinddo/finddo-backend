9f1d99d375184e6839ecc279cb303a86127488a2
Ariel <mileguir23@hotmail.com>
2020-12-22 13:53:12 -0300

    Terceiro teste com o scheduler do Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -83,7 +83,7 @@ Rails.application.routes.draw do
       post 'orders/:id/reschedulings', to: 'orders#create_rescheduling'
       put 'orders/:id/reschedulings/:accepted', to: 'orders#update_rescheduling'
       put 'orders/direct_associate_professional/:id/:professional_id', to: 'orders#direct_associate_professional'
-      get 'expired', to:  'orders#expired_order'
+      get 'expired', to:  'orders#expired_orders'
       put '/orders/rate/:id', to: 'orders#order_rate'
     
       # Adresses
--- lib/tasks/scheduler.rake
+++ lib/tasks/scheduler.rake
@@ -1,4 +1,4 @@
 desc "This task is called by the Heroku scheduler add-on"
 task :expired_orders => :environment do
-  Api::V2::OrdersController.new.expired_orders
+  ServicesModule::V2::OrderService.new.expired_orders
 end
