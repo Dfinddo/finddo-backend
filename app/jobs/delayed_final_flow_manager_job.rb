454695ef7e6225fb7e546d6bbeadf980f3adcccd
Ariel <mileguir23@hotmail.com>
2021-01-13 17:22:29 -0300

    mudando o tempo de espera de final_flow_manager de volta para 15 minutos.

--- app/jobs/delayed_final_flow_manager_job.rb
+++ app/jobs/delayed_final_flow_manager_job.rb
@@ -9,8 +9,8 @@ class DelayedFinalFlowManagerJob < ApplicationJob
     job_name = 'final_flow_manager in 15 minutes for order with id: ' + order_id.to_s
     
     #Precisa ser desativado antes de ativar para que seja feita a recursão
-    Sidekiq.set_schedule(job_name, { 'in' => ['1m'], 'class' => 'FinalFlowManagerSchedulerJob', 'args' => [order_id.to_s, notification_type], 'enabled' => false } )
-    Sidekiq.set_schedule(job_name, { 'in' => ['1m'], 'class' => 'FinalFlowManagerSchedulerJob', 'args' => [order_id.to_s, notification_type], 'enabled' => true } )
+    Sidekiq.set_schedule(job_name, { 'in' => ['15m'], 'class' => 'FinalFlowManagerSchedulerJob', 'args' => [order_id.to_s, notification_type], 'enabled' => false } )
+    Sidekiq.set_schedule(job_name, { 'in' => ['15m'], 'class' => 'FinalFlowManagerSchedulerJob', 'args' => [order_id.to_s, notification_type], 'enabled' => true } )
 
   end
 end
